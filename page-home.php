03af5f8dcf8a5f22ec5d254e39ea47739f0c4c27
Ricardo Veloz <rick@rvlz.io>
2020-08-10 21:07:22 -0500

    add updates

--- page-home.php
+++ page-home.php
@@ -18,4 +18,65 @@
     </div>
   </div>
 </div>
+<div class="container-fluid page-body">
+  <div class="row justify-content-center campaign-description">
+    <div class="col-8">
+      <?php if (get_field('campaign_description')) : ?>
+        <p><?php the_field('campaign_description') ?></p>
+      <?php endif; ?>
+    </div>
+  </div>
+  <div class="row justify-content-center campaign-services">
+    <div class="col-8">
+      <div class="title">
+        <h2>Campaign Services</h2>
+      </div>
+      <div class="items">
+        <ul>
+          <?php
+          if ( have_rows('featured_campaign_services') ):
+            while ( have_rows('featured_campaign_services') ) : the_row();
+            $service = get_sub_field('service');
+          ?>
+            <li>
+              <h3><?php echo esc_html($service->post_title) ?></h3>
+              <p><?php echo esc_html($service->post_content) ?></p>
+              <a href="<?= esc_url(get_permalink($service->ID)) ?>">Learn more</a>
+            </li>
+          <?php
+            endwhile;
+          endif;
+          ?>
+        </ul>
+      </div>
+    </div>
+  </div>
+  <div class="row justify-content-center campaign-works">
+    <div class="col-8">
+      <div class="title">
+        <h2>Our Work</h2>
+      </div>
+      <div class="items">
+        <ul>
+          <?php
+          if ( have_rows('featured_works') ):
+            while ( have_rows('featured_works') ) : the_row();
+            $work = get_sub_field('work');
+            print_r($work);
+          ?>
+            <li>
+              <h3><?php echo esc_html($work->post_title) ?></h3>
+              <p><?php echo esc_html($work->post_content) ?></p>
+              <a href="<?= esc_url(get_permalink($work->ID)) ?>">Learn more</a>
+            </li>
+          <?php
+            endwhile;
+          endif;
+          ?>
+        </ul>
+      </div>
+    </div>
+  </div>
+  <?php get_template_part("template-parts/contact-us") ?>
+</div>
 <?php get_footer(); ?>
---
+++ template-parts/contact-us.php
@@ -0,0 +1,8 @@
+<div class="row justify-content-center contact-us">
+  <div class="col-8">
+      <div class="title">
+        <h2>Ready to get started?<br>Send us a note and let's chat.</h2>
+      </div>
+      <button>Contact Us</button>
+    </div>
+</div>
